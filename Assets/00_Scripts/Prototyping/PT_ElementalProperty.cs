4a67b511e4495f02da24994ce739481a77c76e67
bubblestew95 <bubblestew95@gmail.com>
2025-08-03 23:02:57 +0900

    속성 간 상호작용 테스트 완료

--- Assets/00_Scripts/Prototyping/PT_ElementalProperty.cs
+++ Assets/00_Scripts/Prototyping/PT_ElementalProperty.cs
@@ -16,55 +16,22 @@ public enum ElementalType
 /// </summary>
 public class PT_ElementalProperty : MonoBehaviour
 {
-    [Tooltip("현재 오브젝트의 속성")]
-    public ElementalType currentElement = ElementalType.None;
+    [Tooltip("현재 오브젝트의 속성"), SerializeField]
+    private ElementalType currentElement = ElementalType.None;
 
-    private void OnTriggerEnter(Collider other)
+    [Tooltip("이 오브젝트에 적용될 수 있는 속성들"), SerializeField]
+    private ElementalType[] validElements;
+
+    public void SetElement(ElementalType newElement)
     {
-        PT_ElementalProperty otherElement = other.GetComponent<PT_ElementalProperty>();
-        if (otherElement != null)
+        if (System.Array.Exists(validElements, element => element == newElement))
         {
-            OnElementalCollision(otherElement.currentElement);
-        }
-    }
-
-    /// <summary>
-    /// 다른 속성과 충돌했을 때 호출되는 메서드.
-    /// </summary>
-    /// <param name="collidedElement">충돌한 오브젝트의 속성</param>
-    private void OnElementalCollision(ElementalType collidedElement)
-    {
-        // 여기에 상호작용 로직을 구현하세요.
-        Debug.Log($"Collided with {collidedElement}");
-
-        if (currentElement == collidedElement)
-        {
-            Debug.Log("Same elemental type, no interaction.");
+            currentElement = newElement;
+            Debug.Log($"Element set to: {currentElement}");
         }
         else
         {
-            // 다른 속성과의 상호작용 로직을 구현합니다.
-            switch (currentElement)
-            {
-                case ElementalType.Fire:
-                    if (collidedElement == ElementalType.Water)
-                    {
-                        Debug.Log("Fire extinguished by Water!");
-                    }
-                    break;
-                case ElementalType.Water:
-                    if (collidedElement == ElementalType.Fire)
-                    {
-                        Debug.Log("Water evaporated by Fire!");
-                    }
-                    break;
-                case ElementalType.Wind:
-                    Debug.Log("Wind interacts with " + collidedElement);
-                    break;
-                default:
-                    Debug.Log("No specific interaction defined.");
-                    break;
-            }
+            Debug.Log($"Element {newElement} is not valid for this object.");
         }
     }
 }
